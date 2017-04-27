db9547007501655d557773b8b275034ed3964fa7
David Edward Weirich <weirich.david@gmail.com>
2017-04-27 19:50:50 -0400

    Added test for setting a bit. Changed some names to be more clear.

--- src/bit_field.h
+++ src/bit_field.h
@@ -1,6 +1,6 @@
 #ifndef BIT_FIELD_H
 #define BIT_FIELD_H
 
-void bit_field_set_bit(char *bits, long int bit_to_set);
+void bit_field_set_bit(char *bits, unsigned long int bit_to_set);
 
 #endif // BIT_FIELD_H
--- test/bloom_test.c
+++ test/bloom_test.c
@@ -2,11 +2,22 @@
 
 #include "../src/bit_field.h"
 
-START_TEST(bit_field_set_bit_will_set_the_first_bit) {
+START_TEST(bit_field_set_bit_will_set_the_zeroth_bit) {
 
-    char actual[2] = {0};
+    char actual[2] = {0x00, 0x00};
     long int bit_to_set = 0;
-    char expected[16] = {0x01, 0x00};
+    char expected[2] = {0x01, 0x00}; // 00000001 00000000
+
+    bit_field_set_bit(actual, bit_to_set);
+
+    ck_assert_int_eq(memcmp(expected, actual, 2), 0);
+} END_TEST
+
+START_TEST(bit_field_set_bit_will_set_the_tenth_bit) {
+
+    char actual[2] = {0x00, 0x00};
+    long int bit_to_set = 10;
+    char expected[2] = {0x00, 0x04}; // 00000000 00000100
 
     bit_field_set_bit(actual, bit_to_set);
 
@@ -22,7 +33,8 @@ int main() {
 
     suite_add_tcase(s, tc_bit_field);
 
-    tcase_add_test(tc_bit_field, bit_field_set_bit_will_set_the_first_bit);
+    tcase_add_test(tc_bit_field, bit_field_set_bit_will_set_the_zeroth_bit);
+    tcase_add_test(tc_bit_field, bit_field_set_bit_will_set_the_tenth_bit);
 
     srunner_run_all(sr, CK_NORMAL);
     num_fails = srunner_ntests_failed(sr);
