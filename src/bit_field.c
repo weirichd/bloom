63f24ef2c71b0bce7e169e5cb7409a152a240c5d
David Edward Weirich <weirich.david@gmail.com>
2017-04-27 20:04:41 -0400

    Refactored bit field to use longs instead of chars.

--- src/bit_field.c
+++ src/bit_field.c
@@ -1,9 +1,9 @@
 #include "bit_field.h"
 
-void bit_field_set_bit(char *bits, unsigned long int bit_to_set) {
+void bit_field_set_bit(unsigned long int *bits, unsigned long int bit_to_set) {
 
-    unsigned long int index_in_bits = bit_to_set / 8;
-    unsigned char bit_in_byte_to_set = bit_to_set % 8;
+    unsigned long int index_in_bits = bit_to_set / sizeof(unsigned long);
+    unsigned char bit_in_byte_to_set = bit_to_set % sizeof(unsigned long);
 
     bits[index_in_bits] = 1 << bit_in_byte_to_set;
 }
--- test/bloom_test.c
+++ test/bloom_test.c
@@ -4,20 +4,22 @@
 
 START_TEST(bit_field_set_bit_will_set_the_zeroth_bit) {
 
-    char actual[2] = {0x00, 0x00};
+    unsigned long actual[2] = { };
     long int bit_to_set = 0;
-    char expected[2] = {0x01, 0x00}; // 00000001 00000000
+    unsigned long expected[2] = {1L, 0L};
 
     bit_field_set_bit(actual, bit_to_set);
 
     ck_assert_int_eq(memcmp(expected, actual, 2), 0);
 } END_TEST
 
-START_TEST(bit_field_set_bit_will_set_the_tenth_bit) {
+START_TEST(bit_field_set_bit_will_set_the_hundredth_bit) {
 
-    char actual[2] = {0x00, 0x00};
-    long int bit_to_set = 10;
-    char expected[2] = {0x00, 0x04}; // 00000000 00000100
+    unsigned long actual[2] = { };
+    long int bit_to_set = 100;
+    int shift_ammount = 100 - (8 * sizeof(unsigned long));
+
+    unsigned long expected[2] = {0L, 1L << shift_ammount};
 
     bit_field_set_bit(actual, bit_to_set);
 
@@ -34,7 +36,7 @@ int main() {
     suite_add_tcase(s, tc_bit_field);
 
     tcase_add_test(tc_bit_field, bit_field_set_bit_will_set_the_zeroth_bit);
-    tcase_add_test(tc_bit_field, bit_field_set_bit_will_set_the_tenth_bit);
+    tcase_add_test(tc_bit_field, bit_field_set_bit_will_set_the_hundredth_bit);
 
     srunner_run_all(sr, CK_NORMAL);
     num_fails = srunner_ntests_failed(sr);
