3d60d541273b1073dd8b12e6934d8fb82ee94ece
David Edward Weirich <weirich.david@gmail.com>
2017-05-09 16:34:54 -0400

    Chi^2 tests for Hash functions

--- python/stats_test.py
+++ python/stats_test.py
@@ -4,23 +4,84 @@ import pybloom
 import numpy as np
 import scipy.stats
 
+with open("/usr/share/dict/words") as f:
+    english_words = f.readlines()
+
+with open("random_strings.txt") as f:
+    random_strings = f.readlines()
+
 class StatsTest(unittest.TestCase):
 
-    @unittest.skip("Skipping this test for now since I know it will fail")
-    def test_hash_function_0_passes_chi_square_test_with_P_value_greater_than_point_0_5(self):
-        f = open("/usr/share/dict/words")
-        words = f.readlines()
+    def test_given_english_hash_functions_individually_pass_chi_square_test_with_P_value_greater_than_point_0_5(self):
+        for hash_number in range(20):
+            arr = []
+            for word in english_words:
+                h = pybloom.hash(hash_number, word)
+                arr.append(h)
 
-        hash_number = 0
+            hist, _ = np.histogram(arr, bins=100)
+            chi2, p = scipy.stats.chisquare(hist)
 
+            self.assertGreaterEqual(p, 0.05)
+
+    def test_given_random_strings_hash_functions_individually_pass_chi_square_test_with_P_value_greater_than_point_0_5(self):
+        for hash_number in range(20):
+            arr = []
+            for word in random_strings:
+                h = pybloom.hash(hash_number, word)
+                arr.append(h)
+
+            hist, _ = np.histogram(arr, bins=100)
+            chi2, p = scipy.stats.chisquare(hist)
+
+            self.assertGreaterEqual(p, 0.05)
+
+    def test_given_english_and_random_strings_hash_functions_individually_pass_chi_square_test_with_P_value_greater_than_point_0_5(self):
+        for hash_number in range(20):
+            arr = []
+            for word in english_words + random_strings:
+                h = pybloom.hash(hash_number, word)
+                arr.append(h)
+
+            hist, _ = np.histogram(arr, bins=100)
+            chi2, p = scipy.stats.chisquare(hist)
+
+            self.assertGreaterEqual(p, 0.05)
+
+    def test_given_english_hash_functions_collectively_pass_chi_square_test_with_P_value_greater_than_point_0_5(self):
         arr = []
-        for word in words:
-            h = pybloom.hash(hash_number, word)
-            arr.append(h)
+        for hash_number in range(20):
+            for word in english_words:
+                h = pybloom.hash(hash_number, word)
+                arr.append(h)
 
         hist, _ = np.histogram(arr, bins=100)
         chi2, p = scipy.stats.chisquare(hist)
-        
+
+        self.assertGreaterEqual(p, 0.05)
+
+    def test_given_random_strings_hash_functions_collectively_pass_chi_square_test_with_P_value_greater_than_point_0_5(self):
+        arr = []
+        for hash_number in range(20):
+            for word in random_strings:
+                h = pybloom.hash(hash_number, word)
+                arr.append(h)
+
+        hist, _ = np.histogram(arr, bins=100)
+        chi2, p = scipy.stats.chisquare(hist)
+
+        self.assertGreaterEqual(p, 0.05)
+
+    def test_given_english_and_random_strings_hash_functions_collectively_pass_chi_square_test_with_P_value_greater_than_point_0_5(self):
+        arr = []
+        for hash_number in range(20):
+            for word in english_words + random_strings:
+                h = pybloom.hash(hash_number, word)
+                arr.append(h)
+
+        hist, _ = np.histogram(arr, bins=100)
+        chi2, p = scipy.stats.chisquare(hist)
+
         self.assertGreaterEqual(p, 0.05)
 
 if __name__ == '__main__':
